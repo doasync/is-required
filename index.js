111d48b18295737ebdb13bb943c4de89059a3043
doasync <doasync@gmail.com>
2017-07-24 21:47:32 +0300

    Suppress inspection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function required (paramName) {
   let msg = 'Undefined or missing parameter';
   msg += paramName ? ` "${paramName}"` : '';
   msg += callerName ? ` in function "${callerName}"` : '';
+  // noinspection UnnecessaryLocalVariableJS
   const PARAM_REQUIRED = msg;
   throw new Error(PARAM_REQUIRED);
 }
